8d8a6b9e488276842d3288e8e49db9adfa4f5933
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-12-01 23:39:13 +0530

    #12 schedule task

--- src/main/java/com/rama/backend_spring_boot/component/ScheduledTaskManager.java
+++ src/main/java/com/rama/backend_spring_boot/component/ScheduledTaskManager.java
@@ -1,12 +1,14 @@
 package com.rama.backend_spring_boot.component;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ScheduledFuture;
 
+@Slf4j
 @Component
 public class ScheduledTaskManager {
 
@@ -21,18 +23,18 @@ public class ScheduledTaskManager {
     public void start(Runnable task, long intervalMillis) {
         if (scheduledFuture == null || scheduledFuture.isCancelled()) {
             scheduledFuture = taskScheduler.scheduleAtFixedRate(task, intervalMillis);
-            System.out.println("Task started.");
+            log.info("Task started.");
         } else {
-            System.out.println("Task is already running.");
+            log.info("Task is already running.");
         }
     }
 
     public void stop() {
         if (scheduledFuture != null && !scheduledFuture.isCancelled()) {
             scheduledFuture.cancel(false);
-            System.out.println("Task stopped.");
+            log.info("Task stopped.");
         } else {
-            System.out.println("No running task to stop.");
+            log.info("No running task to stop.");
         }
     }
 
--- src/main/java/com/rama/backend_spring_boot/controller/SchedulerController.java
+++ src/main/java/com/rama/backend_spring_boot/controller/SchedulerController.java
@@ -1,12 +1,14 @@
 package com.rama.backend_spring_boot.controller;
 
 import com.rama.backend_spring_boot.component.ScheduledTaskManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/scheduler")
 public class SchedulerController {
@@ -20,7 +22,7 @@ public class SchedulerController {
 
     @PostMapping("/start")
     public ResponseEntity<String> startTask(@RequestParam long intervalMillis) {
-        Runnable task = () -> System.out.println("Executing task at: " + LocalDateTime.now());
+        Runnable task = () -> log.info("Executing task at: " + LocalDateTime.now());
         taskManager.start(task, intervalMillis);
         return ResponseEntity.ok("Task started with interval: " + intervalMillis + " ms.");
     }
