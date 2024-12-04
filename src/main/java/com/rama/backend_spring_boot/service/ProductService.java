b0a5fa2f541319b06bd46515b2f8b5673336ed7b
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-12-04 21:00:23 +0530

    #10 spring boot starter cache

--- src/main/java/com/rama/backend_spring_boot/service/ProductService.java
+++ src/main/java/com/rama/backend_spring_boot/service/ProductService.java
@@ -6,13 +6,16 @@ import com.rama.backend_spring_boot.model.Product;
 import com.rama.backend_spring_boot.model.dtos.ProductDTO;
 import com.rama.backend_spring_boot.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -20,14 +23,17 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Cacheable(value = "products", key = "'allProducts'")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    @Cacheable(value = "product", key = "#id")
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product with ID " + id + " not found."));
     }
 
+    @CachePut(value = "product", key = "#product.id")
     public Product createProduct(ProductDTO product) {
         if (productRepository.existsById(product.getId())) {
             throw new ProductAlreadyExistsException("Product with ID " + product.getId() + " already exists.");
@@ -41,6 +47,7 @@ public class ProductService {
         return productRepository.save(newProduct);
     }
 
+    @CachePut(value = "product", key = "#id")
     public Product updateProduct(Long id, Product product) {
         if (productRepository.existsById(id)) {
             product.setId(id);
@@ -49,6 +56,7 @@ public class ProductService {
         throw new ProductNotFoundException("Product with ID " + id + " not found.");
     }
 
+    @CacheEvict(value = "product", key = "#id")
     public void deleteProduct(Long id) {
         if (!productRepository.existsById(id)) {
            throw new ProductNotFoundException("Product with ID " + id + " not found.");
@@ -56,24 +64,24 @@ public class ProductService {
         productRepository.deleteById(id);
     }
 
+    @Cacheable(value = "productsByPrice", key = "#price + '-' + #page + '-' + #size")
     public Page<Product> getPagedProducts(double price, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return productRepository.findByPriceGreaterThan(price, pageable);
     }
 
-    // Method to get paged and sorted products
+    @Cacheable(value = "pagedProducts", key = "#page + '-' + #size + '-' + #sortBy")
     public Page<Product> getPagedAndSortedProducts(int page, int size, String sortBy) {
         // Create a Pageable object with pagination and sorting
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
         return productRepository.findAll(pageable);
     }
 
-    // Method to get paged products filtered by name
+    @Cacheable(value = "productsByName", key = "#name + '-' + #page + '-' + #size")
     public Page<Product> getProductsByName(String name, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return productRepository.findByNameContaining(name, pageable);
     }
-
-
 }
+
 
